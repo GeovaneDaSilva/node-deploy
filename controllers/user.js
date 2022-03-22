f907d6c1df0b8f0ad7683715d6c82bb5b6f7c59a
inmigrausa <96200886+inmigrausa@users.noreply.github.com>
2022-03-22 13:12:13 -0400

    okay

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,7 @@ function getUsers(req, res) {
   try {
      res.json({
        ok: true,
-       msg: 'Okay is working perfect Production everthing okay 100%.!'
+       msg: 'Okay is working perfect!!'
      })
   } catch (error) {
     console.log(error);
