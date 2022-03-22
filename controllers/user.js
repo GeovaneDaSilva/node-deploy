065ce139cca409a461740c9d587c0ebf7e928e99
inmigrausa <96200886+inmigrausa@users.noreply.github.com>
2022-03-22 13:51:53 -0400

    y

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,7 @@ function getUsers(req, res) {
   try {
      res.json({
        ok: true,
-       msg: 'Okay is working perfect!!'
+       msg: 'Okay is working perfect!0000000000!'
      })
   } catch (error) {
     console.log(error);
