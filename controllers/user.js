b3928f69a2da247aaccc4424a9bd7ec8ea5dd484
Geovane <geovaneartedesign@gmail.com>
2022-03-23 20:15:09 -0400

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,7 @@ function getUsers(req, res) {
   try {
      res.json({
        ok: true,
-       msg: 'Perfect deployment 100%'
+       msg: 'Perfect deployment 100% Work with jenkins in the other computer'
      })
   } catch (error) {
     console.log(error);
